404a1cd6b7693fa299f4c68f5b35d0e5adf017b3
Patryk Pluta <plutapatryk@endor.local>
2019-11-07 13:09:52 +0100

    commit

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php 
 require_once('database.php');
+if(isset($_GET['send'])){
+    $send = 'a' .$_GET['send'];
+    switch($send) {
+        case 'ateacher':
+            $data = [
+                'imie' => $_POST['imie'],
+                'nazwisko' => $_POST['nazwisko'],
+                'pesel' => $_POST['pesel']
+            ];
+            $db_conn = dbConnect($db_server,$db_user,$db_name,$db_pass);
+            $sql = 'insert into nauczyciele values (NULL, :imie, :nazwisko, :pesel)';
+            $db_stmt = $db_conn->prepare($sql);
+            $db_stmt->execute($data);
+            $db_conn = null;
+            header('Location: index.php');
+        break;
+    }
+} else{
+    echo 'index send nie istnieje';
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,6 +31,12 @@ require_once('database.php');
     <title>Document</title>
 </head>
 <body>
-    
+<form action="index.php?send=teacher"  method= "post" name="addTeacher" id="addTeacher">
+    <input type="text" name="imie" id="">
+    <input type="text" name="nazwisko" id="">
+    <input type="text" name="pesel" id="">
+    <input type="submit" value ="">
+</form>
+
 </body>
 </html>
